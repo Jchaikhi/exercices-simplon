2957f22aa3a62c5c09340bfd7a8a1138baef685c
Jusitn <chaikhij@gmail.com>
2015-11-26 17:05:11 +0100

    Script Js

---
+++ app.js
@@ -0,0 +1,31 @@
+
+/*------------------------------------------------*/
+
+function compar_chaine(str){
+
+var maj = str.toUpperCase();
+var tag = "<br />";
+
+if (str == maj){
+
+document.writeln(str+'Il sont egale'+tag);
+
+}
+
+else{
+
+document.writeln('Il ne sont pas egale'+tag);
+}
+
+}
+
+compar_chaine("Blalalalala Blalalala");
+
+var une_variable = "jdhfklsdjf ,kdsfjs";
+
+compar_chaine(une_variable);
+compar_chaine("MJLJIHDJSHJ");
+
+/*---------------------------------------------------*/
+
+
